3bbdd9d0788c02b1c7917df6d7b7920589b642a3
srchen17 <118634599+srchen17@users.noreply.github.com>
2023-12-14 04:04:15 -0500

    url change

--- src/users/client.js
+++ src/users/client.js
@@ -7,8 +7,8 @@ const request = axios.create({
 
 
 // export const BASE_API = "https://movie-project-backend.onrender.com"
-export const BASE_API = "http://localhost:4000";
-// export const BASE_API = "https://test-movie-backend.onrender.com";
+// export const BASE_API = "http://localhost:4000";
+export const BASE_API = "https://test-movie-backend.onrender.com";
 
 export const USERS_API = `${BASE_API}/api/users`;
 
